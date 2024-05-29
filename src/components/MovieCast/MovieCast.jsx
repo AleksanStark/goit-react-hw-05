a9bece8734dbf1805ba9bff7b1921c4de1147742
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-05-29 10:44:17 +0900

    update MovieCast.jsx MovieReviews.jsx

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -3,29 +3,53 @@ import MoviesList from "../MoviesList/MoviesList";
 import theMovieDBapi from "../../helpers/movies-api";
 import { useParams } from "react-router-dom";
 import MoviesListItem from "../MoviesListItem/MoviesListItem";
+import { Oval } from "react-loader-spinner";
+import toast, { Toaster } from "react-hot-toast";
 
 const MovieCast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchCast = async () => {
-      const { cast } = await theMovieDBapi.getCastById(movieId);
-      setCast(cast);
+      try {
+        setLoader(true);
+        setError(false);
+
+        const { cast } = await theMovieDBapi.getCastById(movieId);
+        setCast(cast);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
     };
     fetchCast();
   });
   return (
-    <MoviesList>
-      {cast.map((actor) => (
-        <MoviesListItem key={actor.id}>
-          <h3>{actor.name}</h3>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-            alt=""
-          />
-        </MoviesListItem>
-      ))}
-    </MoviesList>
+    <>
+      {loader && <Oval />}
+      {error &&
+        toast.error("Oops something went wrong please reload the page", {
+          position: "top-right",
+        })}
+      <Toaster />
+      {cast.length > 0 && (
+        <MoviesList>
+          {cast.map((actor) => (
+            <MoviesListItem key={actor.id}>
+              <h3>{actor.name}</h3>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                alt=""
+              />
+            </MoviesListItem>
+          ))}
+        </MoviesList>
+      )}
+    </>
   );
 };
 
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -3,27 +3,52 @@ import theMovieDBapi from "../../helpers/movies-api";
 import { useParams } from "react-router-dom";
 import MoviesList from "../MoviesList/MoviesList";
 import MoviesListItem from "../MoviesListItem/MoviesListItem";
+import { Oval } from "react-loader-spinner";
+import toast, { Toaster } from "react-hot-toast";
 
 const MovieReviews = () => {
   const [reviews, setReviews] = useState([]);
   const { movieId } = useParams();
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchReviews = async () => {
-      const { results } = await theMovieDBapi.getReviewsById(movieId);
-      setReviews(results);
+      try {
+        setLoader(true);
+        setError(false);
+        const { results } = await theMovieDBapi.getReviewsById(movieId);
+        setReviews(results);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
     };
     fetchReviews();
   }, [movieId]);
 
   return (
-    <MoviesList>
-      {reviews.map((review) => (
-        <MoviesListItem key={review.id}>
-          <h3>{review.author}</h3>
-          <p>{review.content}</p>
-        </MoviesListItem>
-      ))}
-    </MoviesList>
+    <>
+      {error &&
+        toast.error("Oops something went wrong please try reload the page", {
+          position: "top-right",
+        })}
+      <Toaster />
+      {loader && <Oval />}
+      {reviews.length > 0 ? (
+        <MoviesList>
+          {reviews.map((review) => (
+            <MoviesListItem key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+            </MoviesListItem>
+          ))}
+        </MoviesList>
+      ) : (
+        <p>Not reviews yet</p>
+      )}
+    </>
   );
 };
 export default MovieReviews;
